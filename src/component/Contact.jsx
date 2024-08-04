c5975b0d29147352320df09071d05926d1ad0d8a
ap-pamungkas <agustinusputra94@gmail.com>
2024-08-04 14:42:04 +0700

    first commit

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -7,9 +7,9 @@ function Contact() {
             className="mt-12 py-12 bg-gray-300 rounded-lg p-5 h-[45vh]"
           >
             <div className="text-center">
-              <h2 className="text-3xl font-bold mb-4">Hubungi Kami</h2>
+              <h2 className="text-3xl font-bold mb-4">Contact Me</h2>
               <p className="text-gray-700 mb-6">
-                Kami sangat senang mendengar dari Anda! Jangan ragu untuk
+                Saya sangat senang mendengar dari Anda! Jangan ragu untuk
                 menghubungi kami melalui media sosial atau email.
               </p>
               <button className="px-6 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-700">
